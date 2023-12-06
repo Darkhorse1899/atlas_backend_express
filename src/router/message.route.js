6ebb8def69449902f3dd9c373ebb634598263602
super2058 <superMark2058@gmail.com>
2023-12-06 14:10:57 -0500

    monaaa

--- src/router/message.route.js
+++ src/router/message.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import model from "../model/message.model";
+import nodemailer from "nodemailer";
 
 const router = Router();
 
@@ -8,11 +9,33 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "lee19970108@gmail.com",
+      pass: "ajryssjkdgod1108",
+    },
+  });
+
+  var mailOptions = {
+    from: "lee19970108@gmail.com",
+    to: ["kevin@atlasstudios.com", "hello@atlassacademics.com"],
+    subject: "Sending Email using Node.js",
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
   res.send(await model.create(req.body));
 });
 
 router.post("/view", async (req, res) => {
-    res.send(await model.findByIdAndUpdate(req.query.id, {isview: true}));
-  });
+  res.send(await model.findByIdAndUpdate(req.query.id, { isview: true }));
+});
 
 export default router;
