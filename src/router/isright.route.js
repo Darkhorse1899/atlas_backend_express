1f62030ffa351f4cad4159660fdac033ce417fd3
super2058 <superMark2058@gmail.com>
2023-12-07 10:53:37 -0500

    Fix the adjust mailing method

--- src/router/isright.route.js
+++ src/router/isright.route.js
@@ -8,6 +8,45 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+  const { email, likeItems, dislikeItems, distinction } = req.body;
+
+  const likeItemsArray = JSON.parse(likeItems);
+  const disItemsArray = JSON.parse(dislikeItems);
+
+  var transporter = nodemailer.createTransport({
+    service: "Outlook",
+    auth: {
+      user: "babeengineer@outlook.com",
+      pass: "Notepad++",
+    },
+  });
+
+  var mailOptions = {
+    from: "babeengineer@outlook.com",
+    to: [
+      "kevin@atlasstudios.com",
+      "hello@atlasacademics.com",
+      "smartrichard220@outlook.com",
+    ],
+    subject: "Atlas Contact - Is atlas right for our child?",
+    text: `
+      email: ${email}
+      Are any of the following not being met at your child's current school? (choose all that apply)
+      ${disItemsArray.map((item) => `${item}<br />`)}
+      Which result do you value the most at end of the semester and/or school year that presently not being met by your present school?
+      ${distinction}
+      Would You Like
+      ${likeItemsArray.map((item) => `${item}<br />`)}
+    `,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
   res.send(await model.create(req.body));
 });
 router.get("/file", async (req, res) => {
